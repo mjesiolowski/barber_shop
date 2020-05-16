a9ee214ae3e2c45726bc5a50b6d501250a5cd16c
mjesiolowski <maciej.jesiolowski@gmail.com>
2020-05-16 11:37:17 +0200

    init

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,13 +1,41 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URL, {
-    useNewUrlParser: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }).then(() => {
-    console.log('Succesfully connected to the DB')
-    console.log(process.env.MONGODB_URL)
-  })
+mongoose.connect('mongodb://127.0.0.1:27017/barber-shop-api', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+}).then(() => {
+  console.log('Succesfully connected to the DB');
+  console.log('mongodb://127.0.0.1:27017/barber-shop-api');
+})
   .catch((e) => {
-    console.log(`DB error: ${e}`)
-  })
+    console.log(`DB error: ${e}`);
+  });
+
+
+const User = mongoose.model('User', {
+  name: {
+    type: String,
+  },
+  type: {
+    type: String,
+  },
+  availability: {
+    type: Array,
+    default: [],
+  },
+});
+
+// const Availability = mongoose.model('Availability', {
+
+// });
+
+const me = new User({
+  name: 'barber1',
+  type: 'employee',
+});
+
+me.save()
+  .then((e) => console.log('saved: ', e))
+  .catch((e) => console.log(e));
--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
-const app = require('./server')
+const app = require('./server');
 
-const port = process.env.PORT;
+const port = 3000;
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}!`);
