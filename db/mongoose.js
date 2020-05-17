589763746a36666ee8b8a1954b44c49fa6c36bf6
mjesiolowski <maciej.jesiolowski@gmail.com>
2020-05-17 11:47:42 +0200

    started setting up schemas

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/barber-shop-api', {
   useNewUrlParser: true,
@@ -14,28 +15,106 @@ mongoose.connect('mongodb://127.0.0.1:27017/barber-shop-api', {
   });
 
 
-const User = mongoose.model('User', {
+const utcDate = new Date().toJSON().slice(0, 10).replace(/-/g, '/');
+
+const Barber = mongoose.model('Barber', {
   name: {
     type: String,
+    required: true,
+    trim: true,
   },
-  type: {
+  email: {
     type: String,
+    required: true,
+    trim: true,
+    lowercase: true,
+    validate(value) {
+      if (!validator.isEmail(value)) {
+        throw new Error('Email is invalid');
+      }
+    },
   },
-  availability: {
-    type: Array,
-    default: [],
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 10,
   },
+  availability: [{}],
 });
 
-// const Availability = mongoose.model('Availability', {
+const Availability = mongoose.model('Availability', {
+  date: Date,
+  hours: [{
+    hour: Number,
+    status: String,
+    clientName: String,
+    serviceType: String,
+  }],
+});
 
+const availability1 = new Availability({
+  date: utcDate,
+  hours: [{
+    hour: 800,
+    status: 'occupied',
+    clientName: 'John Dee',
+    serviceType: 'combo',
+  }],
+});
+
+// const availability2 = new Availability({
+//   date: utcDate,
+//   hours: [{
+// hour: 830,
+// status: 'occupied',
+// clientName: 'John Dee',
+// serviceType: 'combo',
+//   }],
 // });
 
-const me = new User({
+const barber1 = new Barber({
   name: 'barber1',
-  type: 'employee',
+  email: 'barber1@barber-shop.pl',
+  password: 'test',
+  availability: [
+    {
+      [utcDate]: {
+        800: {
+          status: 'occupied',
+          clientName: 'John Dee',
+          serviceType: 'combo',
+        },
+      },
+    },
+    {
+      [utcDate]: {
+        830: {
+          status: 'occupied',
+          clientName: 'John Dee',
+          serviceType: 'combo',
+        },
+      },
+    },
+  ],
 });
 
-me.save()
-  .then((e) => console.log('saved: ', e))
-  .catch((e) => console.log(e));
+// availability1.save()
+//   .then((e) => console.log('saved: ', e))
+//   .catch((e) => console.log(e));
+
+// Availability.findOneAndUpdate(
+//   { date: utcDate },
+//   {
+//     $push: {
+//       hours: {
+//         hour: 830,
+//         status: 'occupied',
+//         clientName: 'John Dee',
+//         serviceType: 'combo',
+//       },
+//     },
+//   },
+// )
+//   .then((resp) => console.log(resp))
+//   .catch((e) => console.log(e));
